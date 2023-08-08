30cf10eacff26738ebd3e643ea769a00816b939d
Matheus Gabardo Messias <62657173+matheusgabardo@users.noreply.github.com>
2023-08-07 22:17:05 -0300

    Update installation_script_html.sh

--- installation_script_html.sh
+++ installation_script_html.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-red=tput setaf 1
 green=tput setaf 2
 yellow=tput setaf 3
-blue=tput setaf 4
 reset=tput sgr0
 
 echo "${yellow}Starting...${reset}"
 echo ""
 
-echo "${green}[1/2] Installing APACHE${reset}"
+echo "${green}[1/3] Installing APACHE${reset}"
 echo ""
 sudo apt-get update
 sudo apt-get install apache2 -y 
 
-echo "${green}[2/2] Setting up your HTML files${reset}"
+echo "${green}[2/3] Cloning the repository and setting up your HTML files${reset}"
 echo ""
 sudo rm -rf /var/www/html/*
-echo "${yellow}[QUESTION] Type the path to your HTML directory:${reset}"
+echo "${yellow}[QUESTION] Type the Git repository URL:${reset}"
+read gitRepository
+echo "${yellow}[QUESTION] Type the relative path to your HTML directory inside the repository:${reset}"
 read htmlDirectory
-sudo cp -R $htmlDirectory/* /var/www/html/
+sudo git clone $gitRepository /tmp/repo
+sudo cp -R /tmp/repo/$htmlDirectory/* /var/www/html/
+sudo rm -rf /tmp/repo
 
-echo "${green}Finish${reset}"
+echo "${green}[3/3] Finish${reset}"
+
