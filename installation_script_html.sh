eb6e4d80304962971f01920eecf4358508101c63
Matheus Gabardo Messias <62657173+matheusgabardo@users.noreply.github.com>
2023-08-07 22:25:50 -0300

    Update installation_script_html.sh
    added ssh key config to flow

--- installation_script_html.sh
+++ installation_script_html.sh
@@ -2,6 +2,7 @@
 
 green=tput setaf 2
 yellow=tput setaf 3
+blue=tput setaf 4
 reset=tput sgr0
 
 echo "${yellow}Starting...${reset}"
@@ -11,6 +12,15 @@ echo "${green}[1/3] Installing APACHE${reset}"
 echo ""
 sudo apt-get update
 sudo apt-get install apache2 -y 
+
+echo "${green}[X/X] Generating SSH Key and Adding to SSH-Agent${reset}"
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+echo "${blue}SSH key generated. Please add the following public key to your GitHub account:${reset}"
+cat ~/.ssh/id_rsa.pub
+echo "${yellow}[QUESTION] Type 'YES' if you did that:${reset}"
+read answer
 
 echo "${green}[2/3] Cloning the repository and setting up your HTML files${reset}"
 echo ""
